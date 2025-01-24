5103b5a91dccced855d7b8bf48992f823d4f7f4b
tariqahmed56 <Tariq0056@gmail.com>
2025-01-25 00:07:45 +0500

    Fix deploy issue

--- src/app/admin/addexaminfo/page.js
+++ src/app/admin/addexaminfo/page.js
@@ -1,17 +1,31 @@
 "use client"
 import { supabase } from '@/app/lib/supabaseClient';
 import Tiptap from '@/components/Tiptap'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 
 const AddExamInfo = () => {
-  const [content,setContent] = useState(()=>localStorage.getItem('content') || "");
-  const [title,setTitle] = useState(()=>localStorage.getItem('title') || "");
+  const [content,setContent] = useState(()=>{
+    if(typeof window !== 'undefined'){
+      return localStorage.getItem('content') || ""
+    }
+    else return ""
+  });
+  const [title,setTitle] = useState(()=>{
+    if(typeof window !== 'undefined'){
+      return localStorage.getItem('title') || ""
+    }
+    else return ""
+  });
+  useEffect(()=>{
+    if(typeof window !== 'undefined'){
+      localStorage.setItem('title',title);
+      localStorage.setItem('content',content);
+    }
+  },[title,content])
   const handleSubmit = async () => {
     const {data,error} = await supabase.from("EXAM_INFO").insert([{title,description:content}]);
     if(!error){
-      localStorage.setItem('title',"");
-      localStorage.setItem('content',"");
       setContent("");
       setTitle("");
       toast.success("Exam Information added successfully.");
@@ -25,8 +39,9 @@ const AddExamInfo = () => {
     <form className="w-full mx-auto h-auto gap-2 flex flex-col" onSubmit={(e)=>e.preventDefault()}>
       <h1 className='heading !text-center'>Add New Exam Information</h1>
       <input value={title} onChange={(e)=>{
-        localStorage.setItem('title',e.target.value)
-        setTitle(e.target.value)
+       
+          setTitle(e.target.value)
+      
       }
       } label="Title" minLength={10} placeholder='Type a brief title ...' name="title" required  type="text" className="input !py-2 border !border-gray-200"/>
       <Tiptap content={content} setContent={setContent}/>
